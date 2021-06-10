c94dabce5a5c864cd18614b60f34110f34ad956d
Thyago <t.y_josef7@hotmail.com>
2021-06-10 01:40:52 -0300

    criando componente aluno e profesores

--- Angular/SmartSchool/src/app/app.module.ts
+++ Angular/SmartSchool/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    AlunoComponent,
+      ProfessoresComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ Angular/SmartSchool/src/app/professores/professores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professores',
+  templateUrl: './professores.component.html',
+  styleUrls: ['./professores.component.css']
+})
+export class ProfessoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
